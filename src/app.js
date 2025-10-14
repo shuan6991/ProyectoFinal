88dc4de2d16f912335e16a22bcca86e9fadeeeb0
Georgeshuan Bolaños <georgeshuan6991@gmail.com>
2025-10-14 08:38:54 -0500

    se instala dependencia cors para la aplicacion y se importa en el app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ import routerUser from './routers/usuarios.router.js'
 import routerProduct from './routers/productos.router.js'
 import { iniciarConection } from './conexion/conection.js'
 import { iniciarServidor } from './conexion/server.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 
 iniciarConection()
