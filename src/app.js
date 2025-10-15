f2e6af6ee5239256a5efa3dca88c411a486cdd50
jjuansebarias-CTRL <jjuansebarias@gmail.com>
2025-10-15 18:45:55 -0500

    Se Organizo la app

--- src/app.js
+++ src/app.js
@@ -3,17 +3,33 @@ import bodyParser from 'body-parser'
 import routerUser from './routers/usuarios.router.js'
 import routerProduct from './routers/productos.router.js'
 import { iniciarConection } from './conexion/conection.js'
-import { iniciarServidor } from './conexion/server.js'
+import { envs } from './config/env.js'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
 import cors from 'cors'
+// import { iniciarServidor } from './conexion/server.js'
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 iniciarConection()
+
+
+
+
+app.use(cors());
+const app = express()
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname + `../${envs.publicPath}`)))
+
+
 
 // app.use('/users', routerUser)
 app.use('/products', routerProduct)
 app.use('/users', routerUser)
 
-iniciarServidor(app)
+ app.get('/', (req , res)=>{
+        const indexPath = path.join(__dirname + `../../${publicPath}/login.html`)
+        res.sendFile(indexPath)
+})
+
+    export default app
