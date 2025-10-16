b52e16c1729d9c08f5f5a0e934af7467a66c1133
Georgeshuan Bolaños <georgeshuan6991@gmail.com>
2025-10-15 20:37:02 -0500

    pruebasssss2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { config } from 'dotenv'
 import routerUser from './routers/usuarios.router.js'
 import routerProduct from './routers/productos.router.js'
 import { iniciarConection } from './conexion/conection.js'
@@ -12,29 +13,31 @@ import { envs } from './config/env.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+config()
 
 const app = express()
 
-iniciarConection()
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.json())
 
+
+app.use(express.static(path.join(__dirname, "../public")))
 
 
 // app.use('/users', routerUser)
 app.use('/products', routerProduct)
 app.use('/users', routerUser)
 
+
+iniciarConection()
+
 app.get('/', (req, res) => {
     const indexPath = path.join(__dirname, `../../public/index.html`)
     res.sendFile(indexPath)
 })
 
-// app.get('/', (req, res) => {
-//   res.send('API funcionando ✅');
-// });
 
 
 
