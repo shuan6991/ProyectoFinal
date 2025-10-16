ea7d42c7e5edc1689f2cec6d8811952c09ffda55
Georgeshuan Bolaños <georgeshuan6991@gmail.com>
2025-10-15 19:18:48 -0500

    se organiza el app y se crea archivo vercel

--- src/app.js
+++ src/app.js
@@ -3,17 +3,41 @@ import bodyParser from 'body-parser'
 import routerUser from './routers/usuarios.router.js'
 import routerProduct from './routers/productos.router.js'
 import { iniciarConection } from './conexion/conection.js'
-import { iniciarServidor } from './conexion/server.js'
+// import { iniciarServidor } from './conexion/server.js'
 import cors from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { envs } from './config/env.js'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const { port } = envs
 
 const app = express()
-app.use(cors())
-app.use(bodyParser.json())
 
 iniciarConection()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "../public")))
+
+
 
 // app.use('/users', routerUser)
 app.use('/products', routerProduct)
 app.use('/users', routerUser)
 
-iniciarServidor(app)
+app.get('/', (req, res) => {
+    const indexPath = path.join(__dirname, `../../public/index.html`)
+    res.sendFile(indexPath)
+})
+
+app.listen(port, () => {
+    console.log(`El servidor esta escuchando por el puerto ${port}`)
+})
+
+// iniciarServidor(app)
+
+export default app
